955916fa3d877553f60dc18fb27f27a2657d388f
minsubkim <kpukms21@naver.com>
2018-11-03 02:03:52 +0900

    적 체력, 우리팀 체력 구현
    체력구현

--- 게임/boss.py
+++ 게임/boss.py
@@ -3,9 +3,12 @@ from pico2d import *
 class Boss:
     def __init__(self):
         self.image = load_image('boss.png')
+        self.font = load_font('ENCR10B.TTF', 16)
+        self.HP = 2000
 
     def update(self):
         pass
 
     def draw(self):
         self.image.clip_draw(0, 0, 250, 350, 1455, 350)
+        self.font.draw(1350, 500, '(HP: %3.2f/2000.00)' % self.HP, (255, 0, 0))
--- 게임/tower.py
+++ 게임/tower.py
@@ -3,9 +3,12 @@ from pico2d import *
 class Tower:
     def __init__(self):
         self.image = load_image('tower.png')
+        self.font = load_font('ENCR10B.TTF', 15)
+        self.HP = 1000
 
     def update(self):
         pass
 
     def draw(self):
         self.image.clip_draw(0, 0, 200, 200, 90, 300)
+        self.font.draw(30, 420, '(HP: %3.2f/1000.00)' % self.HP, (0, 0, 255))
