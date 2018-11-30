0f39c94ae0fb250432bc6fa8c2dbe3b5e8c92475
minsubkim <kpukms21@naver.com>
2018-12-01 02:38:47 +0900

    title_state 추가
    title_state 추가

--- 게임/mygame.py
+++ 게임/mygame.py
@@ -1,8 +1,8 @@
 import game_framework
 import pico2d
 
-import main_state
+import title_stage
 
 pico2d.open_canvas(1565, 700,sync=True)
-game_framework.run(main_state)
+game_framework.run(title_stage)
 pico2d.close_canvas()
