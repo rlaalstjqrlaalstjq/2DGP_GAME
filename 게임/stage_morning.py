9b91ba419f43a7037d736a8172fbf5933f391928
minsubkim <kpukms21@naver.com>
2018-12-01 02:43:26 +0900

    배경음악 구현
    배경음악 구현 (촌스러워서 리소스수집후 변경할것)

--- 게임/stage_morning.py
+++ 게임/stage_morning.py
@@ -3,7 +3,9 @@ from pico2d import *
 class Stage_Morning:
     def __init__(self):
         self.image = load_image('stage1.png')
-
+        self.bgm = load_music('grondmusic.mp3')
+        self.bgm.set_volume(50)
+        self.bgm.repeat_play()
     def update(self):
         pass
 
